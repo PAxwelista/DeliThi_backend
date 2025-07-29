1300c93ea6288bc3e63a1820689beac3ff5543b6
madotto.Axel@outlook.com <madotto.axel@outlook.com>
2025-07-29 10:02:42 +0200

    separe dev and prod database

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString =
+    process.env.NODE_ENV === "development"
+        ? process.env.CONNECTION_STRING_DEVELOPMENT
+        : process.env.CONNECTION_STRING_PRODUCTION;
 
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log('Database connected'))
-  .catch(error => console.error(error));
+mongoose
+    .connect(connectionString, { connectTimeoutMS: 2000 })
+    .then(() => console.log("Database connected"))
+    .catch(error => console.error(error));
